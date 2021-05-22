90a76c320cd8e5572484bea1b5935bb3f231c3d7
Billo Diallo <billodiallonet@gmail.com>
2021-05-22 16:06:35 +0300

    Add class playerContainer, constructor

--- src/Classes/players/PlayerContainer.js
+++ src/Classes/players/PlayerContainer.js
@@ -1,10 +1,44 @@
 import Phaser from 'phaser';
 import Player from './Player';
 
-
 const Direction = {
-    RIGHT: 'RIGHT',
-    LEFT: 'LEFT',
-    UP: 'UP',
-    DOWN: 'DOWN',
-  };
+  RIGHT: 'RIGHT',
+  LEFT: 'LEFT',
+  UP: 'UP',
+  DOWN: 'DOWN',
+};
+
+export default class PlayerContainer extends Phaser.GameObjects.Container {
+  constructor(scene, x, y, key, frame, health, maxHealth, id, attackAudio) {
+    super(scene, x, y);
+    this.scene = scene;
+    this.velocity = 160;
+    this.currentDirection = Direction.RIGHT;
+    this.playerAttacking = false;
+    this.flipX = true;
+    this.swordHit = false;
+    this.health = health;
+    this.maxHealth = maxHealth;
+    this.id = id;
+    this.attackAudio = attackAudio;
+
+    this.setSize(64, 64);
+    this.scene.physics.world.enable(this);
+
+    this.body.setCollideWorldBounds(true);
+    this.scene.add.existing(this);
+    this.scene.cameras.main.startFollow(this);
+
+    this.player = new Player(this.scene, 0, 0, key, frame);
+    this.add(this.player);
+
+    this.weapon = this.scene.add.image(40, 0, 'items', 4);
+    this.scene.add.existing(this.weapon);
+    this.weapon.setScale(1.5);
+    this.scene.physics.world.enable(this.weapon);
+    this.add(this.weapon);
+    this.weapon.alpha = 0;
+
+    this.createHealthBar();
+  }
+}
