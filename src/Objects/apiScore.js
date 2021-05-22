028a7c621ef1c6962618fdcab4fb55437e3c8a6e
Billo Diallo <billodiallonet@gmail.com>
2021-05-22 09:33:22 +0300

    Add object postScore

--- src/Objects/apiScore.js
+++ src/Objects/apiScore.js
@@ -3,3 +3,9 @@ import 'regenerator-runtime';
 
 const apiKey = 'Zl4d7IVkemOTTVg2fUdz';
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/scores`;
+
+export const postScore = async (name, s) => {
+    const userScore = {
+      user: name,
+      score: s,
+    };
