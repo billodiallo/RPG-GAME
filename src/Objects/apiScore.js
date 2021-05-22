ba661635798326fe5f6eb89946074e71464200bd
Billo Diallo <billodiallonet@gmail.com>
2021-05-22 09:24:37 +0300

    Add Api and url

---
+++ src/Objects/apiScore.js
@@ -0,0 +1,5 @@
+import 'regenerator-runtime';
+// global variables
+
+const apiKey = 'Zl4d7IVkemOTTVg2fUdz';
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/scores`;
