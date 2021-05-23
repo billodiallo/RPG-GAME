2f0f763a5e27d4732f0880ce0700353eb29da449
Billo Diallo <billodiallonet@gmail.com>
2021-05-23 10:20:14 +0300

    Add Spawnertype objet, randomNumber,getTitleProperty

---
+++ src/GameManager/ChestModel.js
@@ -0,0 +1,17 @@
+export const SpawnerType = {
+  MONSTER: 'MONSTER',
+  CHEST: 'CHEST',
+};
+
+export function randomNumber(min, max) {
+  return Math.floor(Math.random() * max) + min;
+}
+
+export function getTiledProperty(obj, propertyName) {
+  for (let propertyIndex = 0; propertyIndex < obj.properties.length; propertyIndex += 1) {
+    const property = obj.properties[propertyIndex];
+    if (property.name === propertyName) {
+      return property.value;
+    }
+  }
+}
