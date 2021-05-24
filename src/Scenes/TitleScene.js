7d639d1b90f7e9e743355ef68cde84336ab4894d
Billo Diallo <billodiallonet@gmail.com>
2021-05-24 18:56:37 +0300

    Import Phaser, Button,config and add Titlescene class

---
+++ src/Scenes/TitleScene.js
@@ -0,0 +1,8 @@
+import Phaser from 'phaser';
+import Button from '../Objects/Button';
+import config from '../Config/config';
+
+export default class TitleScene extends Phaser.Scene {
+  constructor() {
+    super('Title');
+  }
