2c8c287644ba3ffc3b3fa56b85a6a6c8b9dc8075
Billo Diallo <billodiallonet@gmail.com>
2021-05-24 12:40:08 +0300

    Add spawnObjet,spawnMonster,spawnchest methods

--- src/GameManager/Spawner.js
+++ src/GameManager/Spawner.js
@@ -26,3 +26,34 @@ export default class Spawner {
     }, this.spawnInterval);
     if (this.objectType === SpawnerType.MONSTER) this.moveMonsters();
   }
+
+  spawnObject() {
+    if (this.objectType === SpawnerType.CHEST) {
+      this.spawnChest();
+    } else if (this.objectType === SpawnerType.MONSTER) {
+      this.spawnMonster();
+    }
+  }
+
+  spawnChest() {
+    const location = this.pickRandomLocation();
+    const chest = new ChestModel(location[0], location[1], randomNumber(10, 20), this.id);
+    this.objectsCreated.push(chest);
+    this.addObject(chest.id, chest);
+  }
+
+  spawnMonster() {
+    const location = this.pickRandomLocation();
+    const monster = new MonsterModel(
+      location[0],
+      location[1],
+      randomNumber(10, 20),
+      this.id,
+      randomNumber(0, 20),
+      randomNumber(3, 5),
+      1,
+    );
+    this.objectsCreated.push(monster);
+    this.addObject(monster.id, monster);
+  }
+}
