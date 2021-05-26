3d2105cbfea1354bb11dcf5fb995fb7a99aa72d7
Billo Diallo <billodiallonet@gmail.com>
2021-05-26 19:44:42 +0300

    Rename Preloader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import GameScene from './Scenes/GameScene';
 import BootScene from './Scenes/BootScene';
-import PreloaderScene from './Scenes/PreloaderScene';
+import preloaderScene from './Scenes/preloaderScene';
 import UiScene from './Scenes/UiScene';
 import TitleScene from './Scenes/TitleScene';
 import OptionsScene from './Scenes/OptionsScene';
@@ -16,7 +16,7 @@ class Game extends Phaser.Game {
   constructor() {
     super(config);
     this.scene.add('Boot', BootScene);
-    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Preloader', preloaderScene);
     this.scene.add('Title', TitleScene);
     this.scene.add('Options', OptionsScene);
     this.scene.add('Credits', CreditsScene);
