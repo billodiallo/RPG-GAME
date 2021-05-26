17aacac14578ab38fb5ab7b8d85bc1390276e41b
Billo Diallo <billodiallonet@gmail.com>
2021-05-26 20:04:47 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import Phaser from 'phaser';
 import GameScene from './Scenes/GameScene';
 import BootScene from './Scenes/BootScene';
-/* eslint-disable */
-import PreloaderScene from './Scenes/PreloaderScene';
-/* eslint-disable */
+import preloaderScene from './Scenes/preloaderScene';
 import UiScene from './Scenes/UiScene';
 import TitleScene from './Scenes/TitleScene';
 import OptionsScene from './Scenes/OptionsScene';
@@ -18,7 +16,7 @@ class Game extends Phaser.Game {
   constructor() {
     super(config);
     this.scene.add('Boot', BootScene);
-    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Preloader', preloaderScene);
     this.scene.add('Title', TitleScene);
     this.scene.add('Options', OptionsScene);
     this.scene.add('Credits', CreditsScene);
